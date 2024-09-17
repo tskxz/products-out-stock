b6581a940f168e13388cae23414049f3b5b32289
Tanjil <tanjilkh@gmail.com>
2024-09-17 17:48:40 +0100

    Create Table.jsx

---
+++ src/Table/Table.jsx
@@ -0,0 +1,29 @@
+import styles from './Table.module.css'
+
+function Table(props){
+    const items = props.item
+    const listItems = items.map(item =>
+        <tr key={item.name}>
+            <td>{item.name}</td>
+            <td>{item.price}</td>
+            <td>{item.stock}</td>
+        </tr>
+    );
+
+    return(
+        <table className={styles}>
+            <thead>
+                <tr>
+                    <th>Name</th>
+                    <th>Price</th>
+                    <th>Stock</th>
+                </tr>
+            </thead>
+            <tbody>
+                {listItems}
+            </tbody>
+        </table>
+    )
+}
+
+export default Table
